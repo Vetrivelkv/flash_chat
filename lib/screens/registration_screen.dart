1891c05727665f089e858a0d00c8fe2bf1de68e7
Vetri <37167788+Vetrivelkv@users.noreply.github.com>
2022-07-10 10:16:22 +0530

    firebase_Authentication_Registration_Screen

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -79,7 +79,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                     final newUser = await _auth.createUserWithEmailAndPassword(
                         email: email, password: password);
                     if (newUser != null) {
-                      // Navigator.pushNamed(context, ChatScreen.id);
+                      Navigator.pushNamed(context, ChatScreen.id);
                     }
                   } catch (e) {
                     print(e);
